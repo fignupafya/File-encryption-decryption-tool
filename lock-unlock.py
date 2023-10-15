c9390a52d1a981a8a9dafc1566be38423c40e060
fignupafya <99953314+fignupafya@users.noreply.github.com>
2023-10-15 17:48:43 +0300

    Add files via upload

--- lock-unlock.py
+++ lock-unlock.py
@@ -6,7 +6,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 import platform
 
-SAFE_FILES = ['lock-unlock.py', 'salt.key']
+SAFE_FILES = ['lock-unlock.py', '.salt.key']
 
 
 def hide_file(file_path):
